48679b30be77a0d7ed176acc46559207d97c7c08
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-09-26 22:54:02 +0300

    Completed REST API

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -29,5 +29,45 @@ namespace Play.Catalog.Service.Controllers
         {
             return items.Where(item => item.Id == id).SingleOrDefault();
         }
+
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+
+            items.Add(item);
+
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateItem(Guid id, UpdateItemDto updateItemDto)
+        {
+            var item = items.Where(i => i.Id == id).FirstOrDefault();
+            var updatedItem = item with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price
+            };
+
+
+
+
+            var index = items.FindIndex(item => item.Id == id);
+            items[index] = updatedItem;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(Guid id)
+        {
+            items.RemoveAt(items.FindIndex(item => item.Id == id));
+            return NoContent();
+        }
     }
 }
